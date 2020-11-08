12dd8edd56edd43f85faa53a6fbaf51539088ab9
Sematre <Sematre@gmx.de>
2020-11-08 17:12:38 +0100

    Basic implementation

---
+++ README.md
@@ -0,0 +1,16 @@
+# Backend
+WIP
+
+## Usage
+### Dependencies
+* Python
+* FastAPI
+* Uvicorn
+
+For **Arch Linux** based distros, you can install the `python-fastapi` and the `uvicorn` package. 
+
+### Running it
+1. Clone the repo
+2. Open a terminal and go to the project folder
+3. Run `uvicorn main:app --reload`
+3. Go to http://127.0.0.1:8000/docs
---
+++ main.py
@@ -0,0 +1,45 @@
+from typing import List, Dict, Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+import string
+import random
+
+app = FastAPI()
+
+class Property(BaseModel):
+    id: int
+    name: str
+    value: str
+    display_type: Optional[str]
+    min: Optional[int]
+    max: Optional[int]
+
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str
+    image: str
+    location: str
+    tags: List[int]
+    properties_custom: List[Property] = []
+    properties_internal: List[Property] = []
+    attachments: Dict = []
+    last_edited: int
+    created: int
+
+@app.get("/")
+def root():
+    return {"version": "0.1.0"}
+
+@app.get("/auth")
+def login(username: str, password: str):
+    return {"access_token": ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
